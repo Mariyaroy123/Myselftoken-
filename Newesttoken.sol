04a6ce7db81267e0df0fa2ece1f6f07feb3d2c77
Mariyaroy123 <149995235+Mariyaroy123@users.noreply.github.com>
2023-11-06 14:29:54 +0530

    Newesttoken.sol

--- Newesttoken.sol
+++ Newesttoken.sol
@@ -2,21 +2,21 @@
 pragma solidity 0.8.18;
 contract Newesttoken {
 
-    // public variables here
+   
     string public tokenName = "TOKENMETACRAFTERS";
     string public tokenAbbrv = "TMC";
     uint public totalSupply = 0;
 
-    // mapping variable here
+   
     mapping(address => uint) public balances;
 
-    // mint function
+  
     function mint (address _address,uint _value) public {
         totalSupply += _value;
         balances[_address] += _value;
     }
 
-    // burn function
+    
     function burn (address _address, uint _value) public {
         if (balances[_address] >= _value) {
             totalSupply -= _value;
